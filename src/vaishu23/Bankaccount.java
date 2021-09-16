53c1e21b39b1342ece288e192544097e28daff7f
Vaishnavi Dode <vaishnavidode.23@gmail.com>
2021-09-16 16:06:10 +0530

    Methods Program

---
+++ src/vaishu23/Bankaccount.java
@@ -0,0 +1,39 @@
+package vaishu23;
+public class Bankaccount {
+	
+	static void setDetails(String username, String password) {
+		/*String username1 ="Vaishnavi";
+		String password1 ="hello";
+		System.out.println("username is" +username1);
+		System.out.println("password is"+password1);
+	*/
+	}
+	
+	static void updatePassword(String password1) {
+		
+	}
+	static void displayInfor() {
+		
+	}
+	
+	public static void main(String args[])
+	{
+		String username= " Vaishnavi";
+		String password= " hello";
+		
+		setDetails("Vaishnavi","hello");
+		
+		System.out.println("Username is" +username);
+		System.out.println("Password is" +password);
+		
+
+			
+		updatePassword("hello123");
+		String password11 = "hello123";
+		System.out.println("update Password is"  + password11);
+		
+		displayInfor();
+	}
+}
+
+	
